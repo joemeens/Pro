97610b18a522019c37ee1e0e3705e9970460e62e
joemeens <51346104+joemeens@users.noreply.github.com>
2019-06-23 18:25:43 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,5 +1,10 @@
-num1=int(input())
-product=1
-for i in range(1,num1+1):
-    product=product*i
-print(product)
+n=int(input())
+a=1
+if(n==0):
+    a==1
+else:
+    for i in range(1,n+1):
+        a=a*i
+        i=i+1
+
+print(a)
