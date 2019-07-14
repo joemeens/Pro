6eccfa614a7e2734fe24886e32c1b00243b7cfb9
joemeens <51346104+joemeens@users.noreply.github.com>
2019-07-14 16:22:07 +0530

    Update 32.py

--- 32.py
+++ 32.py
@@ -2,8 +2,8 @@ num1,k=map(int,input().split(" "))
 a1=list(map(int,input().split()))
 for i in a1:
     if(i==k):
-        print("yes")
+        print("Yes")
         break
 else:
-    print("no")
+    print("No")
     
