42d8a8b199022e44cfb4692314ee1230d7b9194a
joemeens <51346104+joemeens@users.noreply.github.com>
2019-07-14 17:35:43 +0530

    Update 34.py

--- 34.py
+++ 34.py
@@ -1,5 +1,4 @@
 name=input()
-if(len(name)>=3):
-    for i in range(0,len(name)):
-        if(i%3==0):
-            print(name[i],end="")
+for i in range(0,len(name)):
+    if(i%3==0):
+        print(name[i],end="")
